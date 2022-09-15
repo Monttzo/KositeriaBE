d9741ffc0c14f121b1ae994bc03782dfacf9d2c3
Daniel Pulido <dpulido59@gmail.com>
2022-09-14 23:37:57 -0500

    add cors configuration

--- KositeriaProject/settings.py
+++ KositeriaProject/settings.py
@@ -33,7 +33,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://192.168.0.5:8080",
     "https://localhost:8080",
     "https://127.0.0.1:8000",
-    "https://192.168.0.5:8080"
+    "https://192.168.0.5:8080",
+    "https://kositeria.herokuapp.com"
 ]
 
 # Application definition
